6c45e5fa4a75b287a255357ea0d787e0cd4986c8
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-21 23:52:01 +0300

    feat add tab switching
    feat add tab switching

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const menu = document.getElementById('menu');
 const iml = document.getElementById('fon-left');
 const imr = document.getElementById('fon-rigth');
+const nav = document.getElementById('nav-item');
+const bord = document.getElementById('id-row');
 const btn = document.getElementById('btn');
 const cl_btn = document.getElementById('cl-btn');
 
@@ -9,29 +11,68 @@ menu.addEventListener('click', (event)=> {
     event.target.classList.add('nav-act');
 });
 
-bord = document.getElementById('id-row');
 bord.addEventListener('click', (event) => {
     bord.querySelectorAll ('img').forEach(el => el.classList.remove('portfolio-img'));
     event.target.classList.add('portfolio-img') 
 });
-
+  
 document.getElementById('btn').onclick = function() {
-    const subject = document.getElementById('subject').Value;
-    const describe = document.getElementById('describe').Value;
-    if ( subject == null) document.getElementById('tm').innerText = subject;
-        else document.getElementById('tm').innerText = 'без темы';
-        if ( subject == null) document.getElementById('ds').innerText = subject;
-        else document.getElementById('ds').innerText = 'без описания';
-    document.getElementById('message-block').classList.remove ('hidden');
+ //btn.addEventListener('click', (event)=> {
+    let nm = document.getElementById('name');
+    let em = document.getElementById('email');
+          
+    if (nm.checkValidity() && em.checkValidity()) {
+        if (subject.value.toString() !== "") subjectImp.innerText = subject.value.toString();
+                else subjectImp.innerText = 'No subject';
+
+        if (describe.value.toString() !== "") textareaImp.innerText = describe.value.toString();
+                else textareaImp.innerText = 'No description';
+                 
+        document.getElementById('message-block').classList.remove('hidden');
+    }
 }
 
 cl_btn.addEventListener('click', ()=> {
-    document.getElementById('subject').innerText = '';
-    document.getElementById('describe').innerText = '';
+    document.getElementById('subject').value = '';
+    document.getElementById('describe').value = '';
+    document.getElementById('name').value = '';
+    document.getElementById('email').value = '';
     document.getElementById('message-block').classList.add('hidden'); 
 });
 
+document.getElementById('but_L').onclick = function () {
+    const el= document.getElementById('slide');
+    let from = 0; 
+    let to = 1020; 
+    let start = new Date().getTime(); 
+    setTimeout(function() {
+        let now = (new Date().getTime()) - from; 
+        let progress = now / 1000; 
+        let result = (to - from) * delta(progress) + from;
+        el.style.left = result + "px";
+        if (progress < 1) 
+                setTimeout(arguments.callee, 10);
+ }, 10);
+    function delta(progress) {
+        return progress;
+    }
+    return false;
+}
+    
+    /*for (let i = 0; i<5000; i++) {
+        document.getElementById('rigth').style.left = ofset*60 - i*0.02 + 'px';
+        document.getElementById('left').style.left = ofset*650 - i*0.02 + 'px';
+    }
+        document.getElementById('left').style.visibility = 'visible';
+    step++;
+ }
 
+ document.getElementById('but_R').onclick = function () {
+    document.getElementById('rigth').style.visibility = 'hidden';
+    document.getElementById('left').style.left = ofset*60 - 60 + 'px';
+    document.getElementById('rigth').style.left = ofset*270 + 'px';
+    document.getElementById('rigth').style.visibility = 'visible';
+    step++;*/
 document.getElementById('hbl').onclick = function () {
    if (iml.style.visibility === 'hidden') {
        iml.style.visibility = 'visible';
@@ -48,19 +89,14 @@ document.getElementById('hbr').onclick = function () {
     }
  }
  
-
-const nav = document.getElementById('nav-item');
 nav.addEventListener('click', (event) => {
+    nav.querySelectorAll('a').forEach(el => el.classList.remove('nav-item-act'));
+    event.target.classList.add('nav-item-act');
     nav.querySelectorAll('a').forEach (el => {
-        var ird = document.getElementById("id-row").children;
-        var i;
-        var count = 2;
-        ird[ird.length-1].src = 'img' + toString(i + count-1) + '.jpg';
-        for ( i = 0; i < ird.length - 1; i++ ) {
-            ird[i].src = 'img' + toString(i + count) + '.jpg';
-            if (count + 1 <= ird.length) count++;
-                else count = 2;
-        };
+        let ird = document.getElementById("id-row");
+        for (let i = ird.children.length; i >= 0; i--) {
+            ird.appendChild(ird.children[Math.random() * i | 0]);
+        } 
     });
 });
 
