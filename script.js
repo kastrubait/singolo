9fa0f0aeee8b72d56a9d92f4b262bab26fd42e75
kastrubait <60570588+kastrubait@users.noreply.github.com>
2020-03-29 23:38:38 +0300

    add solution responsive
    add solution responsive

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 const menu = document.getElementById('menu');
-const iml = document.getElementById('fon-left');
-const imr = document.getElementById('fon-rigth');
+const bmenu = document.getElementById('bmenu');
+const bmenu_act = document.getElementById('bmenu-box');
 const nav = document.getElementById('nav-item');
 const bord = document.getElementById('id-row');
 const btn = document.getElementById('btn');
@@ -11,13 +11,24 @@ menu.addEventListener('click', (event)=> {
     event.target.classList.add('nav-act');
 });
 
+bmenu_act.addEventListener('click', (event)=> {
+    if (bmenu.className == 'b-menu') bmenu.classList.add('b-menu_activ')
+        else bmenu.classList.remove('b-menu_activ');
+    bmenu_act.querySelectorAll('a').forEach(el => el.classList.remove('link_act'));
+    event.target.classList.add('link_act');
+});
+
+document.getElementById('b-menu_butt').onclick = function () {
+    if (bmenu.className == 'b-menu') bmenu.classList.add('b-menu_activ')
+        else bmenu.classList.remove('b-menu_activ');
+}
+
 bord.addEventListener('click', (event) => {
     bord.querySelectorAll ('img').forEach(el => el.classList.remove('portfolio-img'));
     event.target.classList.add('portfolio-img') 
 });
   
 document.getElementById('btn').onclick = function() {
- //btn.addEventListener('click', (event)=> {
     let nm = document.getElementById('name');
     let em = document.getElementById('email');
           
@@ -40,8 +51,46 @@ cl_btn.addEventListener('click', ()=> {
     document.getElementById('message-block').classList.add('hidden'); 
 });
 
+
+document.getElementById('fnl-2').onclick = function () {
+    document.getElementById('fnl-2').style.display = 'none';
+    document.getElementById('fnl-1').style.display = 'block';
+}
+document.getElementById('fnl-1').onclick = function () {
+    document.getElementById('fnl-1').style.display = 'none';
+    document.getElementById('fnl-2').style.display = 'block';
+}
+document.getElementById('fnr-2').onclick = function () {
+    document.getElementById('fnr-2').style.display = 'none';
+    document.getElementById('fnr-1').style.display = 'block';
+}
+document.getElementById('fnr-1').onclick = function () {
+    document.getElementById('fnr-1').style.display = 'none';
+    document.getElementById('fnr-2').style.display = 'block';
+}
+
+
+
+
+nav.addEventListener('click', (event) => {
+    nav.querySelectorAll('a').forEach(el => el.classList.remove('nav-item-act'));
+    event.target.classList.add('nav-item-act');
+    nav.querySelectorAll('a').forEach (el => {
+        let ird = document.getElementById("id-row");
+        for (let i = ird.children.length; i >= 0; i--) {
+            ird.appendChild(ird.children[Math.random() * i | 0]);
+        } 
+    });
+});
+
+
+
+/*
+let step = 0;
+let ofset = 1;
+
 document.getElementById('but_L').onclick = function () {
-    const el= document.getElementById('slide');
+    const el= document.getElementById('fone');
     let from = 0; 
     let to = 1020; 
     let start = new Date().getTime(); 
@@ -58,13 +107,14 @@ document.getElementById('but_L').onclick = function () {
     }
     return false;
 }
-    
+   */ 
     /*for (let i = 0; i<5000; i++) {
         document.getElementById('rigth').style.left = ofset*60 - i*0.02 + 'px';
         document.getElementById('left').style.left = ofset*650 - i*0.02 + 'px';
     }
         document.getElementById('left').style.visibility = 'visible';
     step++;
+
  }
 
  document.getElementById('but_R').onclick = function () {
@@ -73,32 +123,6 @@ document.getElementById('but_L').onclick = function () {
     document.getElementById('rigth').style.left = ofset*270 + 'px';
     document.getElementById('rigth').style.visibility = 'visible';
     step++;*/
-document.getElementById('hbl').onclick = function () {
-   if (iml.style.visibility === 'hidden') {
-       iml.style.visibility = 'visible';
-   } else { 
-    iml.style.visibility = 'hidden';
-   }
-}
-
-document.getElementById('hbr').onclick = function () {
-    if (imr.style.visibility === 'hidden') {
-        imr.style.visibility = 'visible';
-    } else { 
-     imr.style.visibility = 'hidden';
-    }
- }
- 
-nav.addEventListener('click', (event) => {
-    nav.querySelectorAll('a').forEach(el => el.classList.remove('nav-item-act'));
-    event.target.classList.add('nav-item-act');
-    nav.querySelectorAll('a').forEach (el => {
-        let ird = document.getElementById("id-row");
-        for (let i = ird.children.length; i >= 0; i--) {
-            ird.appendChild(ird.children[Math.random() * i | 0]);
-        } 
-    });
-});
 
 
 
